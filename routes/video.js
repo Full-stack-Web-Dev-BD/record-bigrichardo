37fe6246748f254c072e77247e1274ba75c6e771
Alamin <sai.amaraneni94@gmail.com>
2020-12-08 10:13:18 +0600

    done

--- routes/video.js
+++ routes/video.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/videos')
+        cb(null, './uploads/video')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + file.originalname+'.mp4')
